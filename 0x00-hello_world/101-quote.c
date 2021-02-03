5cb2414262432581292f2a53686591ed9f13639d
King-Kingah <kingajames2015@gmail.com>
2021-02-03 20:55:57 +0000

    updated 6,7,& 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main- Entrypoint
  *
- * Return: Always 0 (Success)
+ * Return: 1 (Error)
  */
-fn main()
+int main(void)
 {
-  eprintln!("and that piece of art if useful\" - Dora Korpar, 2015-10-19\n");
-
-  return(1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
