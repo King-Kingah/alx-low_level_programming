930c8eeff18b5c8dc93842d93110019d303d316a
King-Kingah <kingajames2015@gmail.com>
2021-02-03 20:45:42 +0000

    updated scripts and advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-/**
- * main - main block
- * Return: 1
- */
-int main(void)
-{
-  int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
-  write(2,
-	"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-	s);
-  return (1);
- }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+fn main()
+{
+  eprintln!("and that piece of art if useful\" - Dora Korpar, 2015-10-19\n");
+
+  return(1);
+}
