3ebaf9c50950d7ae60706ad177a6618a0875ed4a
King-Kingah <kingajames2015@gmail.com>
2021-02-03 20:14:36 +0000

    added description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-  return 0;
+ printf("with proper grammar, but the outcome is a piece of art,");
+ return (0);
 }
