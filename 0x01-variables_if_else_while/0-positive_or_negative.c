15cba72994d813ff05654b29e7cd7d2fb7f28149
King-Kingah <kingajames2015@gmail.com>
2021-02-04 20:29:48 +0000

    iter1: deleted comments

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
@@ -15,7 +13,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
+
 if (n > 0)
 {
   printf("%i is positive\n", n);
