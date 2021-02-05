8055fd756d9a9565d2803afe1153980bccc25c0a
King-Kingah <kingajames2015@gmail.com>
2021-02-05 15:56:15 +0000

    corrected indentation in advanced tasks

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,30 +8,30 @@
 
 int main(void)
 {
-  int i, j, k;
+int i, j, k;
 
-  i = 0;
+i = 0;
 
-  while (i < 100)
-    {
-      j = i % 10; /* singles digit */
-      k = i / 10; /* doubles digit */
+while (i < 100)
+{
+j = i % 10; /* singles digit */
+k = i / 10; /* doubles digit */
 
-      if (k < j)
-	{
-	  putchar(k + '0');
-	  putchar(j + '0');
+if (k < j)
+{
+putchar(k + '0');
+putchar(j + '0');
 
-	  if (i < 89)
-	    {
-	      putchar(44);
-	      putchar(32);
-	    }
-	}
+if (i < 89)
+{
+putchar(44);
+putchar(32);
+}
+}
 
-      i++;
-    }
-  putchar('\n');
+i++;
+}
+putchar('\n');
 
-  return (0);
+return (0);
 }
