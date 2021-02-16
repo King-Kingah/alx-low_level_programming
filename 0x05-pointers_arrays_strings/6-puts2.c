343603e8a41f3fe4750125a2b4a27946b9670581
King-Kingah <kingajames2015@gmail.com>
2021-02-16 20:52:23 +0000

    line formatting

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,11 @@
 
 void puts2(char *str)
 {
-  int i;
+int i;
 
-  for (i = 0; *(str + i) != '\0'; i = i + 2)
-    {
-      _putchar(*(str + i));
-    }
-  _putchar('\n');
+for (i = 0; *(str + i) != '\0'; i = i + 2)
+{
+_putchar(*(str + i));
+}
+_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-  int i;
+int i;
 
-  for (i = 0; *(src + i) != '\0'; i++)
-    {
-      *(dest + i) = *(src + i);
-    }
+for (i = 0; *(src + i) != '\0'; i++)
+{
+*(dest + i) = *(src + i);
+}
 
- return (dest);
+return (dest);
 }
