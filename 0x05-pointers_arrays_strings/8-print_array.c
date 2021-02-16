3c6bbe361dd56859c4cd9d5161369a105f34ab3e
King-Kingah <kingajames2015@gmail.com>
2021-02-16 01:14:37 +0000

    elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers,
+ * @a: integer
+ * @n: integer
+ */
+
+void print_array(int *a, int n)
+{
+  int i;
+
+  for (i = 0; i < n; i++)
+    {
+      printf("%d", a[i]);
+      if (i != n - 1)
+	{
+	  printf(", ");
+	}
+    }
+  printf("\n");
+}
