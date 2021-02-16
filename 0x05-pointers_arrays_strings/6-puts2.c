2aa447de9b8f63c204715c804cc94351412a342e
King-Kingah <kingajames2015@gmail.com>
2021-02-16 00:31:18 +0000

    puts2 fxn

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ * @str: character
+ */
+
+void puts2(char *str)
+{
+  int i;
+
+  for (i = 0; *(str + i) != '\0'; i = i + 2)
+    {
+      _putchar(*(str + i));
+    }
+  _putchar('\n');
+}
