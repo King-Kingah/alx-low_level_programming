f17df4f1aab6178652712a9bd2812133ee807471
King-Kingah <kingajames2015@gmail.com>
2021-02-19 19:56:15 +0000

    update 02

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,3 +6,16 @@
  *                                                     
  *                                                            
  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+
+for (; n > i; i++)
+dest[i] = '\0';
+
+return (dest);
+}
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,3 +6,16 @@
  *                                                     
  *                                                            
  */
+
+void reverse_array(int *a, int n)
+{
+int i;
+int h;
+
+for (i = 0; i < (n / 2); i++)
+{
+h = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = h;
+}
+}
