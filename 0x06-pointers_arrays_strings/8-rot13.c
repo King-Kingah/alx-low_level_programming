584401d98b38da459d68cc12153b9e87086b958a
King-Kingah <kingajames2015@gmail.com>
2021-02-19 20:18:40 +0000

    update 03

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,17 +9,17 @@
 
 char *rot13(char *s)
 {
-  int i;
-  char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-  char storel[] = "nopqrstuvwxyzabcdefghijklm";
+int i;
+char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+char storel[] = "nopqrstuvwxyzabcdefghijklm";
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
-	{
-	  s[i] = (s[i] - 65 > 25) ?
-	    storel[s[i] - 97] : storeh[s[i] - 65];
-	}
-    }
-  return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+{
+s[i] = (s[i] - 65 > 25) ?
+storel[s[i] - 97] : storeh[s[i] - 65];
+}
+}
+return (s);
 }
