30bf6911dc9a6e5bc551276622b0d21a02330c98
King-Kingah <kingajames2015@gmail.com>
2021-02-19 19:50:49 +0000

    update 011

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-include "holberton.h"
+#include "holberton.h"
 
 /**                                           
  * _strncat - concatenates two strings using at most `n` bytes
@@ -9,15 +9,13 @@ include "holberton.h"
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-int j;
+int i, c;
 
-for (i = 0; dest[i] != '\0'; i++)
+for (i = 0; dest[i] != '\0'; i++);
+
+for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
 {
-}
-for (j = 0; j < n && src[j] != '\0'; j++)
-{
-dest[i + j] = src[j];
+dest[i] = src[c];
 }
 return (dest);
 }
