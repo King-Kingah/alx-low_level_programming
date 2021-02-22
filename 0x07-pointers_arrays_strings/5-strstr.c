02fa92e77ee24f4604f686fb811df52aeeba9318
King-Kingah <kingajames2015@gmail.com>
2021-02-22 20:46:35 +0000

    mismatch fixed

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,26 +7,27 @@
  * Return: a pointer to the beginning of the located substring, or NULL if the substring is not found.
  */
 
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
-  int i, j;
-  char *p;
+  int i, j, c;
 
   i = 0;
-  while (s[i] != '\0')
+  c = 0;
+  while (haystack[i] != '\0')
     {
       j = 0;
-      while (accept[j] != '\0')
+      while (needle[j + c] != '\0' && haystack[i + c] != '\0'
+	     && needle[j + c] == haystack[i + c])
 	{
-	  if (accept[j] == s[i])
-	    {
-	      p = &s[i];
-	      return (p);
-	    }
-	  j++;
+	  if (haystack[i + c] != needle[j + c])
+	    break;
+	  c++;
 	}
+      if (needle[j + c] == '\0')
+	return (&haystack[i]);
+      j++;
       i++;
     }
 
-  return (0);
+  return (NULL);
 }
