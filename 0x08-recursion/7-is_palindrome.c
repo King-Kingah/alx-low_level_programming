b2b23d2e25393a5b89ca2270e49fcefa53a99da4
King-Kingah <kingajames2015@gmail.com>
2021-02-25 20:37:44 +0000

    reverted changes

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
-/*
+/**
  * is_palindrome -  returns 1 if a string is a palindrome and 0 if not.
  * @s: string
+ * Return: 1 if str is palindrome, 0 if not
  */
 
 int is_palindrome(char *s)
@@ -12,14 +13,14 @@ int length;
 length = get_length(s) - 1;
 
 return (my_pal(s, --length));
-}}
+}
 
 int get_length(char *s)
 {
 if (*s == '\0')
 return (1);
 else
-    return (1 + get_length(++s));
+return (1 + get_length(++s));
 }
 
 int my_pal(char *s, int l)
@@ -29,8 +30,8 @@ if (*s == *(s + l))
 if (l <= 0)
 return (1);
 else
-	return (my_pal(++s, l - 2));
+return (my_pal(++s, l - 2));
 }
-  else
-    return (0);
+else
+return (0);
 }
