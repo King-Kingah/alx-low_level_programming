bec86ea6613e06ee439de99f1f30e7e85dd75d62
King-Kingah <kingajames2015@gmail.com>
2021-02-25 20:33:49 +0000

    reverted changes

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -3,7 +3,6 @@
 /*
  * is_palindrome -  returns 1 if a string is a palindrome and 0 if not.
  * @s: string
- * Return: 1 if palindrome, 0 if not
  */
 
 int is_palindrome(char *s)
@@ -12,39 +11,26 @@ int length;
 
 length = get_length(s) - 1;
 
-return (call_sall(s, --length));
-}
-
-/**
- * get_length - obtain string length 
- * @s: string 
- * Return: length of string
- */
+return (my_pal(s, --length));
+}}
 
 int get_length(char *s)
 {
 if (*s == '\0')
 return (1);
 else
-return (1 + get_length(++s));
+    return (1 + get_length(++s));
 }
 
-/**
- * call_sall - recursively checks for palindrome
- * @s: string
- * @l: string length
- * Return: 1 for present palindrome, 0 if absent
- */
-
-int call_sall(char *s, int l)
+int my_pal(char *s, int l)
 {
 if (*s == *(s + l))
 {
 if (l <= 0)
 return (1);
 else
-return (call_sall(++s, l - 2));
+	return (my_pal(++s, l - 2));
 }
-else
-return (0);
+  else
+    return (0);
 }
