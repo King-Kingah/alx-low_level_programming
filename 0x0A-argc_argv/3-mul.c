8014ae77f7b3ab854f468716743609538a27741b
King-Kingah <kingajames2015@gmail.com>
2021-02-28 20:11:38 +0000

    multiply ints

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc:
+ * @argv:
+ * Return: 1 if erroneous, 0 if correct
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+
+if (argv[1] && argv[2])
+{
+printf("%d\n", atoi(argv[1] * atoi(argv[2]));
+return (0);
+}
+else
+printf("Error\n");
+
+return (0);
+}
