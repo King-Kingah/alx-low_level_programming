be4a72b59faed7d5db47dfd5487b5894be493054
King-Kingah <kingajames2015@gmail.com>
2021-02-28 19:29:14 +0000

    first correction

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - prints program name
+ * @argc: an integer type of arguments that contains the number of arguments
+ * @argv: an array of character pointer that contains all parameters
+ * Return: Nothing
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+
+_putchar("%s\n", argv[0]);
+
+return (0);
+}
