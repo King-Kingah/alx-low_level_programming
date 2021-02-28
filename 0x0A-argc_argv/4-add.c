88d12974c9acc32c5fc73496b9fea3102ce9a1e8
King-Kingah <kingajames2015@gmail.com>
2021-02-28 20:45:21 +0000

    updated advanced

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: arg count
+ * @argv: arg vector and array of strs
+ * Return: 1 if erroneous, 0 if correct
+ */
+
+int main(int argc, char *argv[])
+{
+int sum, i;
+char *p;
+int num;
+
+sum = 0;
+if (argv > 1)
+{
+for (i = 1; argv[i]; i++)
+{
+num = strtol(argv[i], &p, 10);
+if (!*p)
+sum += num;
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+printf("%d\n", sum);
+
+return (0);
+}
