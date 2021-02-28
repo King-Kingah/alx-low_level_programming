a6a6800930f97ba4ea3175b398880f999f72094d
King-Kingah <kingajames2015@gmail.com>
2021-02-28 20:57:53 +0000

    updated advanced

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,3 +1,39 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+* main - prints the minimum number of coins to make change for an amount of money.
+* @argc:
+* @argv:
+* Return: 
+*
+*/
+
+int main(int argc, char *argv[])
+{
+  int count;
+  int num;
+
+  if (argc != 2)
+    {
+      printf("Error\n");
+      return (1);
+    }
+  num = atoi(argv[1]);
+  for (count = 0; num > 0; count++)
+    {
+      if (num >= 25)
+	num = num - 25;
+      else if (num >= 10)
+	num = num - 10;
+      else if (num >= 5)
+	num = num - 5;
+      else if (num >= 2)
+	num = num - 2;
+      else
+	num = num - 1;
+    }
+  printf("%d\n", count);
+  return (0);
+}
