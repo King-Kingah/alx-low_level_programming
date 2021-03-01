ada066e02145c2c12980ff99aa6691d4c2f67ce6
King-Kingah <kingajames2015@gmail.com>
2021-03-01 16:07:21 +0000

    betty aligned

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,25 +11,25 @@
 
 int main(int argc, char *argv[])
 {
-  int total, i;
-  char *p;
-  int num;
+int total, i;
+char *p;
+int num;
 
-  total = 0;
-  if (argc > 1)
-    {
-      for (i = 1; argv[i]; i++)
-	{
-	  num = strtol(argv[i], &p, 10);
-	  if (!*p)
-	    total += num;
-	  else
-	    {
-	      printf("Error\n");
-	      return (1);
-	    }
-	}
-    }
-  printf("%d\n", total);
-  return (0);
+total = 0;
+if (argc > 1)
+{
+for (i = 1; argv[i]; i++)
+{
+num = strtol(argv[i], &p, 10);
+if (!*p)
+total += num;
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+printf("%d\n", total);
+return (0);
 }
