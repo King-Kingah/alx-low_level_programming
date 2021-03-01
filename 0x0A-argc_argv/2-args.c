b75f0ccd274989744c2007b5ad43601b91a4b910
King-Kingah <kingajames2015@gmail.com>
2021-03-01 16:34:36 +0000

    betty appropriate

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,18 +3,17 @@
 
 /**
  * main - prints all given arguments given through command line
- * @argc: which stores the total number of arguments 
+ * @argc: which stores the total number of arguments
  * @argv: which stores the array of strings that mean all arguments
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-int i;
-for(i = 0; i < argc; i++);
-{
-printf("%s\n", argv[i]);
-}
+int i = 0;
 
-return(0);
+while (i < argc)
+printf("%s\n", argv[i++]);
+
+return (0);
 }
