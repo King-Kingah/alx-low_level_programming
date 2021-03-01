f88069dcec4f0085a080c9bca3bf76b9d913ef4c
King-Kingah <kingajames2015@gmail.com>
2021-03-01 16:18:31 +0000

    betty appropriate

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,37 +3,36 @@
 #include <stdlib.h>
 
 /**
-* main - prints the minimum number of coins to make change for an amount of money.
-* @argc:
-* @argv:
-* Return: 
-*
+* main - prints the make change for an amount of money.
+* @argc: arg counter
+* @argv: arg vector
+* Return: returns 1
 */
 
 int main(int argc, char *argv[])
 {
-  int count;
-  int num;
+int count;
+int num;
 
-  if (argc != 2)
-    {
-      printf("Error\n");
-      return (1);
-    }
-  num = atoi(argv[1]);
-  for (count = 0; num > 0; count++)
-    {
-      if (num >= 25)
-	num = num - 25;
-      else if (num >= 10)
-	num = num - 10;
-      else if (num >= 5)
-	num = num - 5;
-      else if (num >= 2)
-	num = num - 2;
-      else
-	num = num - 1;
-    }
-  printf("%d\n", count);
-  return (0);
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+num = atoi(argv[1]);
+for (count = 0; num > 0; count++)
+{
+if (num >= 25)
+num = num - 25;
+else if (num >= 10)
+num = num - 10;
+else if (num >= 5)
+num = num - 5;
+else if (num >= 2)
+num = num - 2;
+else
+num = num - 1;
+}
+printf("%d\n", count);
+return (0);
 }
