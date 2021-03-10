ffe00e2f3f449090dced2722101608e5a6282de6
King-Kingah <kingajames2015@gmail.com>
2021-03-10 20:58:23 +0000

    updated header files

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,7 +9,7 @@
  */
 int op_add(int a, int b)
 {
-  return (a + b);
+return (a + b);
 }
 
 /**
@@ -20,7 +20,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-  return (a - b);
+return (a - b);
 }
 
 /**
@@ -31,7 +31,7 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-  return (a * b);
+return (a * b);
 }
 
 /**
@@ -42,12 +42,12 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-  if (b == 0)
-    {
-      printf("Error\n");
-      exit(100);
-    }
-  return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
 }
 
 /**
@@ -58,10 +58,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-  if (b == 0)
-    {
-      printf("Error\n");
-      exit(100);
-    }
-  return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
