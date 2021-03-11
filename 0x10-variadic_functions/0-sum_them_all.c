4dd125f00bf3353a2abd57fb572a8ae22cd3bcb8
King-Kingah <kingajames2015@gmail.com>
2021-03-11 20:37:00 +0000

    reassigned variables task 0 & advanced

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,21 +1,21 @@
 #include "variadic_functions.h"
+
 /**
- * sum_them_all - return the sum of all parameters
- * @n: number of arguments passed to function
- * Return: sum of parameters
+ * sum_them_all - return the sum of all its parameters
+ * @n: number of extra params
+ * Return: total sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i;
-int sum = 0;
-va_list list;
+va_list sumlist;
+int sum;
 
-va_start(list, n);
-for (i = 0; i < n; i++)
-{
-sum += va_arg(list, int);
-}
-va_end(list);
+va_start(sumlist, n);
+for (i = 0, sum = 0; i < n; i++)
+sum += va_arg(sumlist, int);
+
+va_end(sumlist);
 
 return (sum);
 }
