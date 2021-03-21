2434dfe886f5fabf5eb1548b7d1e189fc094fd89
King-Kingah <kingajames2015@gmail.com>
2021-03-21 03:44:20 +0000

    initial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *_isupper - check for uppercase character
+ *@c: integer
+ *
+ *Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+    return (0);
+}
