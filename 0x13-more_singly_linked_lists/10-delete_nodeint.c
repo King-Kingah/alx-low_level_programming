0e3888177748526e033d0f33831bc8cd05993959
King-Kingah <kingajames2015@gmail.com>
2021-03-24 20:06:59 +0000

    updated after PLD

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,32 +8,32 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-  listint_t *current;
-  listint_t *hold;
-  unsigned int i;
+listint_t *current;
+listint_t *hold;
+unsigned int i;
 
-  i = 0;
-  current = *head;
-  if (*head == NULL)
-    return (-1);
+i = 0;
+current = *head;
+if (*head == NULL)
+return (-1);
 
-  while (i < index)
-    {
-      i++;
-      hold = current;
-      if (current->next)
-	current = current->next;
-      else
-	return (-1);
-    }
+while (i < index)
+{
+i++;
+hold = current;
+if (current->next)
+current = current->next;
+else
+return (-1);
+}
 
-  if (index == 0)
-    *head = current->next;
-  else if (current->next)
-    hold->next = current->next;
-  else
-    hold->next = NULL;
+if (index == 0)
+*head = current->next;
+else if (current->next)
+hold->next = current->next;
+else
+hold->next = NULL;
 
-  free(current);
-  return (1);
+free(current);
+return (1);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * insert_nodeint_at_index - function that inserts a new node at given position
  * @head: pointer to a pointer to head
@@ -6,44 +7,45 @@
  * @n: value to store in node
  * Return: the address of the new node, or NULL if it failed
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-  listint_t *new;
-  listint_t *current;
-  unsigned int i = 0;
+listint_t *new;
+listint_t *current;
+unsigned int i = 0;
 
-  if (head == NULL)
-    return (NULL);
+if (head == NULL)
+return (NULL);
 
-  current = *head;
-  for (i = 1; current && i < idx; i++)
-    {
-      current = current->next;
-      if (current == NULL)
-	return (NULL);
-    }
+current = *head;
+for (i = 1; current && i < idx; i++)
+{
+current = current->next;
+if (current == NULL)
+return (NULL);
+}
 
-  new = malloc(sizeof(listint_t));
-  if (new == NULL)
-    {
-      free(new);
-      return (NULL);
-    }
-  new->n = n;
-  if (idx == 0)
-    {
-      *head = new;
-      new->next = current;
-    }
-  else if (current->next)
-    {
-      new->next = current->next;
-      current->next = new;
-    }
-  else
-    {
-      new->next = NULL;
-      current->next = new;
-    }
-  return (new);
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+{
+free(new);
+return (NULL);
+}
+new->n = n;
+if (idx == 0)
+{
+*head = new;
+new->next = current;
+}
+else if (current->next)
+{
+new->next = current->next;
+current->next = new;
+}
+else
+{
+new->next = NULL;
+current->next = new;
+}
+return (new);
 }
